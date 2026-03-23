b8fa752b9ec502327f411ef272fedb24632eec81
ArunimaBhattacharyya <arunima4002.bh@gmail.com>
2026-03-23 15:59:31 +0530

    Initial commit

---
+++ src/main/java/com/example/HMS_Sprint/HmsSprintApplication.java
@@ -0,0 +1,13 @@
+package com.example.HMS_Sprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsSprintApplication.class, args);
+	}
+
+}
