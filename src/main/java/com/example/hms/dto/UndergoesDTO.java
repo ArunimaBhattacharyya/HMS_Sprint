8e44cd4c8b23baa4f07a5599ac484ecb4830c7a2
Laffy <bidwattam04@gmail.com>
2026-03-27 14:29:15 +0530

    Added UndergoesDTO

---
+++ src/main/java/com/example/hms/dto/UndergoesDTO.java
@@ -0,0 +1,33 @@
+package com.example.hms.dto;
+
+import java.time.LocalDateTime;
+
+public class UndergoesDTO {
+
+    private int patientId;
+    private int procedureId;
+    private int stayId;
+    private int physicianId;
+    private int nurseId;
+    private LocalDateTime date;
+
+    public UndergoesDTO() {}
+
+    public int getPatientId() { return patientId; }
+    public void setPatientId(int patientId) { this.patientId = patientId; }
+
+    public int getProcedureId() { return procedureId; }
+    public void setProcedureId(int procedureId) { this.procedureId = procedureId; }
+
+    public int getStayId() { return stayId; }
+    public void setStayId(int stayId) { this.stayId = stayId; }
+
+    public int getPhysicianId() { return physicianId; }
+    public void setPhysicianId(int physicianId) { this.physicianId = physicianId; }
+
+    public int getNurseId() { return nurseId; }
+    public void setNurseId(int nurseId) { this.nurseId = nurseId; }
+
+    public LocalDateTime getDate() { return date; }
+    public void setDate(LocalDateTime date) { this.date = date; }
+}
