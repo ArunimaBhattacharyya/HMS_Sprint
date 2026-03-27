50f819ed9c423e308e05419a311e5ab30fcbb08b
Laffy <bidwattam04@gmail.com>
2026-03-27 12:02:24 +0530

    Modified Physician

--- src/main/java/com/example/hms/entity/Physician.java
+++ src/main/java/com/example/hms/entity/Physician.java
@@ -47,14 +47,14 @@ public class Physician {
     )
     private List<Department> departments;
 
-    // Many-to-Many with Procedures
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(
-            name = "trained_in",
-            joinColumns = @JoinColumn(name = "Physician"),
-            inverseJoinColumns = @JoinColumn(name = "Treatment")
-    )
-    private List<Procedure> procedures;
+//    // Many-to-Many with Procedures
+//    @ManyToMany(fetch = FetchType.LAZY)
+//    @JoinTable(
+//            name = "trained_in",
+//            joinColumns = @JoinColumn(name = "Physician"),
+//            inverseJoinColumns = @JoinColumn(name = "Treatment")
+//    )
+//    private List<Procedure> procedures;
 
     // Constructors
     public Physician() {}
