a998a761c48404438d427c02f6a94e14208dbd17
ArunimaBhattacharyya <arunima4002.bh@gmail.com>
2026-03-28 11:18:29 +0530

    medication modified

--- src/main/java/com/example/hms/repository/MedicationRepository.java
+++ src/main/java/com/example/hms/repository/MedicationRepository.java
@@ -1,7 +1,6 @@
 package com.example.hms.repository;
 
 import com.example.hms.entity.Medication;
-import com.example.hms.entity.Nurse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
